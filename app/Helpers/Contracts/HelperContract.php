aad5e913125cb3fd32259e30cde600c029608a8c
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-04-29 17:22:25 +0100

    Add files via upload

--- app/Helpers/Contracts/HelperContract.php
+++ app/Helpers/Contracts/HelperContract.php
@@ -4,12 +4,7 @@ namespace App\Helpers\Contracts;
 Interface HelperContract
 {
         public function sendEmail($to,$subject,$data,$view,$type);
-        public function sendEmailSMTP($data,$view,$type);     
-        public function clearTokens();
-        public function getClient();
-        public function updateClient($data);
-        public function getAccessToken();
-        public function getHardLink($state);
-        public function sendRequest($data);
+        public function sendEmailSMTP($data,$view,$type);            
+        public function addToData($row);
 }
  ?>
